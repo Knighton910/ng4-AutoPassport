fafb9cc6ee71e019fb0386f0f9ad759bd71151e4
kelvin knigton <keltheceo@gmail.com>
2017-05-31 13:32:30 -0700

    added routes: #working: implementing ng/material next

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { NgModule } from '@angular/core';
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CoverpageComponent } from './coverpage/coverpage.component';
+import { CarsComponent } from './cars/cars.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoverpageComponent,
+    CarsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'cover', pathMatch: 'full' },
+      { path: 'cover', component: CoverpageComponent },
+      { path: 'cars', component: CarsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/coverpage/coverpage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coverpage',
+  templateUrl: './coverpage.component.html',
+  styleUrls: ['./coverpage.component.css']
+})
+export class CoverpageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
