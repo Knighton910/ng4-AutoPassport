aa20dc702d3cc9420203b600e7ff580c98f967fb
🦁 Kelvin-Knighton <keltheceo@gmail.com>
2018-03-14 14:45:13 -0700

    finished with app & ready to setup deployment steps

--- src/app/cars/car-thumbnail.component.ts
+++ src/app/cars/car-thumbnail.component.ts
@@ -4,13 +4,15 @@ import {Component, Input} from '@angular/core';
   selector: 'app-car-thumbnail',
   styleUrls: ['car-thumbnail.css'],
   template: `
-    <div class="forefront">
-      <img class="autoImg" [src]="car.imageUrl">
-      <h2 class="name">{{car.name}}</h2>
-      <div class="price">Price: \${{car.price}}</div>
-      <div>Top Speed: {{car.topSpeed}}</div>
-      <div>MSRP: $\{{car.msrp}}</div>
-      <button class="btn" [routerLink]="['/cars', car.id]">Lease Me</button>
+    <div class="parentForefront">
+      <div class="forefront">
+        <img class="autoImg" [src]="car.imageUrl">
+        <h2 class="name">{{car.name}}</h2>
+        <div class="price">Price: \${{car.price}}</div>
+        <div>Top Speed: {{car.topSpeed}}</div>
+        <div>MSRP: $\{{car.msrp}}</div>
+        <button class="btn" [routerLink]="['/cars', car.id]">Lease Me</button>
+      </div>
     </div>
   `
 })
